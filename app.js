60adc44a45d74fb87b12499b1ecea459748f2e39
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2018-04-18 12:12:25 -0700

    This was a button event, mouse tracking, and website linking excercise

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 new Vue({
 	el:'#vue-app',
 	data: {
-		name:'Daniel',
-		job: 'Developer'
+		age:25,
+		x:0,
+		y:0
 	},
 	methods: {
-		greet: function(time){
-			return 'Good ' + time;
+		add: function(inc){
+			this.age += inc;
+		},
+		subtract: function(dec){
+			this.age -= dec;
+		},
+		updateXY: function(event){
+			this.x = event.offsetX;
+			this.y = event.offsetY;
 		}
+
 	}
 });
