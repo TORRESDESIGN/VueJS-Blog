2b7af83d99d6a2d38d6dbdde58d4c5ed93dd8807
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2018-04-21 12:45:13 -0700

    Data binding excercise

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 new Vue({
 	el:'#vue-app',
 	data: {
-		
+		name: '',
+		age: ''
 	},
 	methods: {
-		logName: function(){
-			console.log('you entered your name');
-		},
-		logAge: function(){
-			console.log('you entered your age');
-		}
+		
 	}
 });
