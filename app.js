ee4f981b88b52b8adc15c5099801eb3a5d01013e
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2018-04-21 14:40:36 -0700

    This was a computed properties excercise.

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 new Vue({
 	el:'#vue-app',
 	data: {
-		name: '',
-		age: ''
+		age:20,
+		a:0,
+		b:0
 	},
 	methods: {
-		
+		//adding "()" at the end of function name in the index file makes them read the methods
+		/*addToA: function(){
+			console.log("addToA")
+			return this.a + this.age;
+		},
+		addToB: function(){
+			console.log("addToB")
+			return this.b + this.age;
+		}*/
+	},
+	computed: {
+		//removing "()" at the end of function name in the index file makes them read the methods
+		addToA: function(){
+			console.log("addToA")
+			return this.a + this.age;
+		},
+		addToB: function(){
+			console.log("addToB")
+			return this.b + this.age;
+		}
 	}
 });
