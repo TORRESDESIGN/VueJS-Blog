70e9d8dbbdf9e3318ebca80489fd1c860bb70abb
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2018-04-21 17:47:46 -0700

    This was a dynamic CSS excercise

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 new Vue({
 	el:'#vue-app',
 	data: {
-		age:20,
-		a:0,
-		b:0
+		available: false,
+		nearby: false
 	},
 	methods: {
-		//adding "()" at the end of function name in the index file makes them read the methods
-		/*addToA: function(){
-			console.log("addToA")
-			return this.a + this.age;
-		},
-		addToB: function(){
-			console.log("addToB")
-			return this.b + this.age;
-		}*/
+		
 	},
 	computed: {
-		//removing "()" at the end of function name in the index file makes them read the methods
-		addToA: function(){
-			console.log("addToA")
-			return this.a + this.age;
-		},
-		addToB: function(){
-			console.log("addToB")
-			return this.b + this.age;
+		compClasses: function(){
+			return{
+				available: this.available,
+				nearby: this.nearby
+			}
 		}
 	}
 });
