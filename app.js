4a581c339d1599a83b4bf432d77250cf2c41998d
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2018-04-25 20:01:40 -0700

    This excercise is about creating components and modifying that data with a button.

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
-var one = new Vue({
-	el:'#vue-app-one',
-	data: {
-		title:'Vue App One'
+Vue.component('greeting', {
+	template:'<p>Hey there, I am {{ name }}.<button v-on:click="changeName">Change name</button></p>',
+	data: function(){
+		return{
+			name: 'Yoshi'
+		}
 	},
-	methods: {
-		
-	},
-	computed: {
-		greet: function(){
-			return 'Hello from app one';
+	methods:{
+		changeName:function(){
+			this.name='Mario';
 		}
 	}
 });
 
-var two = new Vue({
-	el:'#vue-app-two',
-	data: {
-		title:'Vue App Two'
-	},
-	methods: {
-		changeTitle: function(){
-			one.title = "Title changed";
-		}
-		
-	},
-	computed: {
-		greet: function(){
-			return 'Hello from app two';
-		}
-		
-	}
+new Vue({
+	el:'#vue-app-one'
 });
 
-two.title = "Changed from outside";
+new Vue({
+	el:'#vue-app-two'
+});
